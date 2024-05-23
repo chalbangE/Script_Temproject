5049b962757360f254af8f601c495cfbc0d3a7a9
chalbangE <yubin41029@naver.com>
2024-05-24 04:44:13 +0900

    프로그램 제목, 부제목 띄우기
    밑에 엄마가 시킨 심부름은 랜덤으로 품목이 바뀜

--- Main.py
+++ Main.py
@@ -1,11 +1,37 @@
 from tkinter import *
+import tkinter.font as tkFont
+import random
+
+W_WIDTH = 1000
+W_HEIGHT = 800
 
 class MainGUI:
     def __init__(self):
         window = Tk()
         window.title('오늘 할 일 : 장 보기')
-        self.canvas = Canvas(window,bg='white',width=1000,height=800)
+
+        Title_font = tkFont.Font(family="와구리체 TTF", size=20)
+        Main_font = tkFont.Font(family="UhBee Seulvely", size=15)
+        print(tkFont.families())
+
+        try:
+            with open('list.txt', 'r', encoding='utf-8') as fp:
+                self.words = fp.read().split()
+                print(self.words)  # 파일에서 읽은 단어 리스트 출력
+        except UnicodeDecodeError as e:
+            print(f"파일 못 열었다... {e}")
+
+        self.canvas = Canvas(window,bg='white',width=W_WIDTH,height=W_HEIGHT)
         self.canvas.pack()
+        self.canvas.create_line(280, 80, W_WIDTH - 280, 80, tags="shape")
+        self.canvas.create_text(W_WIDTH / 2, 20, text='오늘 할 일 : 장 보기', font=Title_font)
+
+        if random.randint(0, 1) == 0:
+            self.canvas.create_text(W_WIDTH / 2, 55, text='엄마가 시킨 심부름 : ' + self.words[random.randint(0, len(self.words) - 1)]
+                                                          + ', '+ self.words[random.randint(0, len(self.words) - 1)], font=Main_font)
+        else:
+            self.canvas.create_text(W_WIDTH / 2, 55, text='엄마가 시킨 심부름 : ' + self.words[random.randint(0, len(self.words) - 1)]
+                                                          + ', '+ self.words[random.randint(0, len(self.words) - 1)] + ', '+ self.words[random.randint(0, len(self.words) - 1)], font=Main_font)
 
         window.mainloop()
 
