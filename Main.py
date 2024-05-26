a97fa98850ac617da8c7621fcb1cea0ccd369c7c
chalbangE <yubin41029@naver.com>
2024-05-26 19:55:30 +0900

    GIF 클래스 추가, 띄우기 완료

--- Main.py
+++ Main.py
@@ -3,6 +3,7 @@ import tkinter.font as tkFont
 import random
 import Product
 import Store
+from PIL import Image, ImageTk
 
 W_WIDTH = 1000
 W_HEIGHT = 800
@@ -16,12 +17,35 @@ s_area_code = Store.LoadAreaCode()                  # 업체 업태 코드 (편�
 s_area_detail_code = Store.LoadAreaDetailCode()     # 업체 지역 코드 (서울, 광주...)
 
 
+class GIF:
+    def __init__(self, window, x, y, width, height):
+        self.gif = Image.open("gif/cat2.gif")
+        self.gif_frames = []
+
+        for frame in range(0, self.gif.n_frames):
+            self.gif.seek(frame)
+            self.gif_frames.append(ImageTk.PhotoImage(self.gif.copy()))
+
+        self.label = Label(window)
+        self.label.place(x=x, y=y, width=width, height=height)  # 원하는 위치와 크기로 설정
+
+    def update_label(self, frame):
+        gif_frame = self.gif_frames[frame]
+        self.label.config(image=gif_frame)
+        self.label.image = gif_frame
+
+    def animate(self, window, current_frame=0):
+        frame_count = len(self.gif_frames)
+        self.update_label(current_frame)
+        current_frame = (current_frame + 1) % frame_count
+        window.after(100, lambda: self.animate(window, current_frame))
+
 class MainGUI:
     def __init__(self):
         window = Tk()
         window.title('오늘 할 일 : 장 보기')
 
-        Title_font = tkFont.Font(family="와구리체 TTF", size=25)
+        Title_font = tkFont.Font(family="와구리체 TTF", size=30)
         Subtitle_font = tkFont.Font(family="UhBee Seulvely", size=15)
 
         try:
@@ -32,16 +56,18 @@ class MainGUI:
 
         self.canvas = Canvas(window,bg='white',width=W_WIDTH,height=W_HEIGHT)
         self.canvas.pack()
-        self.canvas.create_line(280, 80, W_WIDTH - 280, 80, tags="shape")
-        self.canvas.create_text(W_WIDTH / 2, 25, text='오늘 할 일 : 장 보기', font=Title_font)
+        self.canvas.create_line(280, 100, W_WIDTH - 280, 100, tags="shape")
+        self.canvas.create_text(W_WIDTH / 2, 35, text='오늘 할 일 : 장 보기', font=Title_font)
 
         if random.randint(0, 1) == 0:
-            self.canvas.create_text(W_WIDTH / 2, 60, text='엄마가 시킨 심부름 : ' + self.words[random.randint(0, len(self.words) - 1)]
+            self.canvas.create_text(W_WIDTH / 2, 75, text='엄마가 시킨 심부름 : ' + self.words[random.randint(0, len(self.words) - 1)]
                                                           + ', '+ self.words[random.randint(0, len(self.words) - 1)], font=Subtitle_font)
         else:
-            self.canvas.create_text(W_WIDTH / 2, 60, text='엄마가 시킨 심부름 : ' + self.words[random.randint(0, len(self.words) - 1)]
+            self.canvas.create_text(W_WIDTH / 2, 75, text='엄마가 시킨 심부름 : ' + self.words[random.randint(0, len(self.words) - 1)]
                                                           + ', '+ self.words[random.randint(0, len(self.words) - 1)] + ', '+ self.words[random.randint(0, len(self.words) - 1)], font=Subtitle_font)
 
-        window.mainloop()
+        gif_instance = GIF(window, x=20, y=20, width=147, height=80)
+        gif_instance.animate(window)
 
+        window.mainloop()
 MainGUI()
