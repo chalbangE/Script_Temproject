2ee8b0053915b58817d41613dc3aeb3cc741f707
chalbangE <yubin41029@naver.com>
2024-05-27 02:22:48 +0900

    다크모드 구현 완료

--- Main.py
+++ Main.py
@@ -41,6 +41,22 @@ class GIF:
         window.after(100, lambda: self.animate(window, current_frame))
 
 class MainGUI:
+    def Dark_mod_button_click(self):
+        if self.show_mod == 'black':
+            self.show_mod = 'white'
+            self.dark_mod_button['image'] = self.dark_mod_off_image
+            self.canvas.itemconfig("subtitle", fill='black')
+            self.canvas.itemconfig("title_line", fill='black')
+            self.canvas.itemconfig("title", fill='black')
+            self.canvas.itemconfig("background", fill='white')
+        elif self.show_mod == 'white':
+            self.show_mod = 'black'
+            self.dark_mod_button['image'] = self.dark_mod_on_image
+            self.canvas.itemconfig("subtitle", fill='white')
+            self.canvas.itemconfig("title_line", fill='white')
+            self.canvas.itemconfig("title", fill='white')
+            self.canvas.itemconfig("background", fill='black')
+
     def __init__(self):
         window = Tk()
         window.title('오늘 할 일 : 장 보기')
@@ -54,20 +70,38 @@ class MainGUI:
         except UnicodeDecodeError as e:
             print(f"파일 못 열었다... {e}")
 
-        self.canvas = Canvas(window,bg='white',width=W_WIDTH,height=W_HEIGHT)
+        self.canvas = Canvas(window, bg='white', width=W_WIDTH, height=W_HEIGHT)
         self.canvas.pack()
-        self.canvas.create_line(280, 100, W_WIDTH - 280, 100, tags="shape")
-        self.canvas.create_text(W_WIDTH / 2, 35, text='오늘 할 일 : 장 보기', font=Title_font)
+        self.canvas.create_rectangle(0, 0, W_WIDTH, W_HEIGHT, tags="background")
+        self.canvas.create_line(280, 100, W_WIDTH - 280, 100, tags="title_line")
+        self.canvas.create_text(W_WIDTH / 2, 35, tags="title", text='오늘 할 일 : 장 보기', font=Title_font)
+
 
         if random.randint(0, 1) == 0:
-            self.canvas.create_text(W_WIDTH / 2, 75, text='엄마가 시킨 심부름 : ' + self.words[random.randint(0, len(self.words) - 1)]
-                                                          + ', '+ self.words[random.randint(0, len(self.words) - 1)], font=Subtitle_font)
+            self.canvas.create_text(W_WIDTH / 2, 75, tags="subtitle", text='엄마가 시킨 심부름 : ' + self.words[random.randint(0, len(self.words) - 1)]\
+                            + ', '+ self.words[random.randint(0, len(self.words) - 1)], font=Subtitle_font)
         else:
-            self.canvas.create_text(W_WIDTH / 2, 75, text='엄마가 시킨 심부름 : ' + self.words[random.randint(0, len(self.words) - 1)]
-                                                          + ', '+ self.words[random.randint(0, len(self.words) - 1)] + ', '+ self.words[random.randint(0, len(self.words) - 1)], font=Subtitle_font)
+            self.canvas.create_text(W_WIDTH / 2, 75, tags="subtitle", text='엄마가 시킨 심부름 : ' + self.words[random.randint(0, len(self.words) - 1)]\
+                            + ', '+ self.words[random.randint(0, len(self.words) - 1)] + ', '+ self.words[random.randint(0, len(self.words) - 1)], font=Subtitle_font)
+
+        self.show_mod = 'white'
+        # 다크모드 이미지 로드
+        self.dark_mod_on_image = Image.open("img/white_cat.jpg")
+        self.dark_mod_off_image = Image.open("img/black_cat.jpg")
+
+        # 이미지를 Tkinter로 변환
+        self.dark_mod_on_image = ImageTk.PhotoImage(self.dark_mod_on_image)
+        self.dark_mod_off_image = ImageTk.PhotoImage(self.dark_mod_off_image)
+
+        # 버튼 생성 및 이미지 설정
+        self.dark_mod_button = Button(window, image=self.dark_mod_off_image, command=self.Dark_mod_button_click)
+        self.dark_mod_button.place(x=180, y=15)
 
         gif_instance = GIF(window, x=20, y=20, width=147, height=80)
         gif_instance.animate(window)
 
         window.mainloop()
+
+
+
 MainGUI()
